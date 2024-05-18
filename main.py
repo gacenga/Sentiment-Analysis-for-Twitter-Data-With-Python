43a593dcabb313a61d9be9d51ac156e073edc843
gacenga_ <122737849+gacenga@users.noreply.github.com>
2024-05-19 00:40:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import clean_tweets
 import sentiment_analysis
 import visualize_sentiments
 
-tweet = collect_tweets('2023 Toyota Probox Van NHP160V', 200)
-tweet['clean_text'] = tweet['text'].apply(clean_tweets)
-tweet['sentiment'] = tweet['clean_text'].apply(get_sentiment)
+tweet = tweets.collect_tweets('2023 Toyota Probox Van NHP160V', 200)
+tweet['clean_text'] = tweet['text'].apply(vlean_tweets.clean_tweets)
+tweet['sentiment'] = tweet['clean_text'].apply(sentiment_analysis.get_sentiment)
 
-sentiment_distribution(tweet)
+visualize_sentiments.sentiment_distribution(tweet)
 
 tweet['time_created'] = pd.to_datetime(tweet['time_created'])
 tweet.set_index('time_created', inplace=True)
 sentiment_time = tweet['sentiment'].resample('H').value_counts().unstack().fillna(0)
 
-sentiment_timeseries(sentiment_time)
+visualize_sentiments.sentiment_timeseries(sentiment_time)
