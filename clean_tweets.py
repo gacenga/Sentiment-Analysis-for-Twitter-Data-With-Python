bb16343992409fbceae9afdcc34d2c0183e5db04
gacenga_ <122737849+gacenga@users.noreply.github.com>
2024-05-19 09:53:49 +0300

    Update clean_tweets.py

--- clean_tweets.py
+++ clean_tweets.py
@@ -1,6 +1,7 @@
 import re
 
 def clean_tweets(text):
+    """removes hashtags,mentions and special characters"""
     text = re.sub(r'http\S+|www\S+|https\S+', '', text, flags=re.MULTILINE)
     text = re.sub(r'\@\w+|\#','', text)
     text = re.sub(r'[^\w\s]', '', text)
