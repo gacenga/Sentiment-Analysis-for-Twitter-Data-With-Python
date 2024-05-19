601812ab2c9eb6f340f1291b6e0793e3c9a523f3
gacenga_ <122737849+gacenga@users.noreply.github.com>
2024-05-19 09:59:03 +0300

    Update visualize_sentiments.py

--- visualize_sentiments.py
+++ visualize_sentiments.py
@@ -1,17 +1,26 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
+import to_excel
 
 def sentiment_distribution(dataframe):
+    """creates histogram and saves it into an excel file"""
     sns.countplot(x='sentiment', data=dataframe)
     plt.title('sentiment distribution for tweets')
     plt.xlabel('sentiment')
     plt.ylabel('Number of Tweets')
-    plt.show()
+    plot_filename = 'sent_dist.png'
+    plt.savefig(plot_filename)
+    plt.close()
+    to_excel.to_excel(plot_filename, 'E5')
 
 def sentiment_timeseries(sentiment_time):
+    """creates time series of sentiment over time and saves it in an excel file"""
     sentiment_time.plot(kind='line', figsize=(12, 6))
     plt.title('Sentiment over Time')
     plt.xlabel('time')
     plt.ylabel('Number of Tweets')
-    plt.show()
+    plot_filename = 'sent_timeseries.png'
+    plt.savefig(plot_filename)
+    plt.close()
+    to_excel.to_excel(plot_filename, 'E20')
     
