37f9a138254c4640722ce6655e007256b0a0c5bc
gacenga_ <122737849+gacenga@users.noreply.github.com>
2024-05-19 10:17:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sentiment_analysis
 import visualize_sentiments
 
 tweet = tweets.collect_tweets('2023 Toyota Probox Van NHP160V', 20)
-tweet['clean_text'] = tweet['text'].apply(vlean_tweets.clean_tweets)
+tweet['clean_text'] = tweet['text'].apply(clean_tweets.clean_tweets)
 tweet['sentiment'] = tweet['clean_text'].apply(sentiment_analysis.get_sentiment)
 
 visualize_sentiments.sentiment_distribution(tweet)
