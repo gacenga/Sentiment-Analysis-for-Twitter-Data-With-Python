ad368b1e8253c0239d4cbe6ca7f3946f473e6f19
gacenga_ <122737849+gacenga@users.noreply.github.com>
2024-05-19 09:50:55 +0300

    to_excel

---
+++ to_excel.py
@@ -0,0 +1,20 @@
+from openpyxl import Workbook, load_workbook
+from openpyxl.drawing.image import Image
+import 
+
+def to_excel(path, cell_no):
+    excel = 'sentiment_plots.xlsx'
+    if os.path.exists(excel):
+        workbook = load_workbook(excel)
+    else:
+        workbook = Workbook()
+        workbook.active.title = "sentiment_plots"
+    sheet = workbook.active
+
+    #Insert image into the sheet
+    img = Image(path)
+    img.anchor = cell_no
+    sheet.add_image(img)
+
+    #Save excel file
+    workbook.save(excel)os
