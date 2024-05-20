8f89b88207e5a6075444b58603e9b656daba505d
gacenga_ <122737849+gacenga@users.noreply.github.com>
2024-05-20 11:57:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,20 @@ import tweets
 import clean_tweets
 import sentiment_analysis
 import visualize_sentiments
+import to_excel
 
 try:
     tweet = tweets.collect_tweets('2023 Toyota Probox Van NHP160V', 20)
     tweet['clean_text'] = tweet['text'].apply(clean_tweets.clean_tweets)
     tweet['sentiment'] = tweet['clean_text'].apply(sentiment_analysis.get_sentiment)
 
-    visualize_sentiments.sentiment_distribution(tweet)
+    sent_dist_png, sent_dist_html = visualize_sentiments.sentiment_distribution(tweet)
 
     tweet['time_created'] = pd.to_datetime(tweet['time_created'])
     tweet.set_index('time_created', inplace=True)
     sentiment_time = tweet['sentiment'].resample('H').value_counts().unstack().fillna(0)
 
-    visualize_sentiments.sentiment_timeseries(sentiment_time)
+    sent_time_png, sent_time_html = visualize_sentiments.sentiment_timeseries(sentiment_time)
+    to_excel.to_excel([sent_dist_png, sent_time_png], [sent_dist_html, sent_time_html])
 except Exception as e:
     print(f'An error has occured: {e}')
