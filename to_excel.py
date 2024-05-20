303f3b1e1355f27cb1f23b45cb17ec333c6ecef3
gacenga_ <122737849+gacenga@users.noreply.github.com>
2024-05-20 11:50:44 +0300

    Update to_excel.py

--- to_excel.py
+++ to_excel.py
@@ -2,7 +2,7 @@ from openpyxl import Workbook, load_workbook
 from openpyxl.drawing.image import Image
 import os
 
-def to_excel(png_path, html_path):
+def to_excel(png_paths, html_paths):
     """Saves visualizations into an excel file"""
     excel = 'sentiment_plots.xlsx'
     if os.path.exists(excel):
@@ -12,13 +12,15 @@ def to_excel(png_path, html_path):
         workbook.active.title = "sentiment_plots"
     sheet = workbook.active
 
-    #Insert images and links into the sheet
-    img = Image(png_path)
-    cell = f'A{1 + (i * 20)}'
-    sheet.add_image(img, cell)
-    sheet[f'A{2 + (i * 20)}'] = f'Interactive Plot {i + 1}'
-    sheet[f'A{2 + (i * 20)}'].hyperlink = html_path
-    sheet[f'A{2 + (i * 20)}'].style = "Hyperlink"
+    paths = list(zip(png_paths, html_paths))
+    for i, (png_path, html_path) in enumerate(zip(png_paths, html_paths)):
+        #Insert images and links into the sheet
+        img = Image(png_path)
+        cell = f'A{1 + (i * 20)}'
+        sheet.add_image(img, cell)
+        sheet[f'A{2 + (i * 20)}'] = f'Interactive Plot {i + 1}'
+        sheet[f'A{2 + (i * 20)}'].hyperlink = html_path
+        sheet[f'A{2 + (i * 20)}'].style = "Hyperlink"
 
     #Save excel file
     workbook.save(excel)
